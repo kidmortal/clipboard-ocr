c0fcd05a92295e6e2b892eb0a8516d95370175e9
kidmortal <kidmortal@gmail.com>
2024-02-20 22:27:19 -0300

    add placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { createWorker } from "tesseract.js";
 import "./App.css";
+import { When } from "./components/When";
 
 function App() {
   const [imageBlob, setImageBlob] = useState<Blob>();
@@ -33,12 +34,33 @@ function App() {
   return (
     <>
       <div>
-        <img
-          style={{ width: "100%", height: "60vh" }}
-          src={imageBlob && URL.createObjectURL(imageBlob)}
-          className="logo react"
-          alt="React logo"
-        />
+        <When value={!!imageBlob}>
+          <img
+            style={{ width: "100%", height: "60vh" }}
+            src={imageBlob && URL.createObjectURL(imageBlob)}
+            alt="Ocr image"
+          />
+        </When>
+
+        <When value={!imageBlob}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              border: "1px solid red",
+              height: "60vh",
+              justifyContent: "center",
+              alignItems: "center",
+              marginBottom: "1rem",
+            }}
+          >
+            <h1 style={{ margin: "0 3rem" }}>
+              {" "}
+              Control + V to paste any clipboard image
+            </h1>
+          </div>
+        </When>
+
         <textarea style={{ width: "100%", height: "10rem" }} value={text} />
       </div>
     </>
